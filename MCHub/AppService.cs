464eae979e652e29a5ba3f679783d1a4e26ca929
mpcdigitize <plawrow@hotmail.com>
2017-11-19 19:40:44 -0500

    add AppService class

---
+++ MCHub/AppService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMCHub.Data;
+
+namespace MCHub
+{
+   public  class AppService
+   {
+        private AppRepo _repo;
+
+        public AppService ()
+        {
+
+            _repo = new AppRepo;    
+        }
+
+
+
+        public static IEnumerable<MediaItem> GetMediaItems()
+        { 
+                
+            return 
+        }
+
+
+   }
+}

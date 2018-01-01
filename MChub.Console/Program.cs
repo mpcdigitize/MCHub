95de231b6e0d1c36fc4cfb606953f4ecb3f2c7cb
mpcdigitize <plawrow@hotmail.com>
2018-01-01 18:42:40 -0500

    import data to database

--- MChub.Console/Program.cs
+++ MChub.Console/Program.cs
@@ -14,38 +14,38 @@ namespace MCHub
 
             var context = new AppContext();
 
-            //var repo = new Repository();
+            var repo = new Repository();
 
-            //var parser = new ShellParser();
+            var parser = new ShellParser();
 
-            //var repoService = new LocalRepositoryService(repo, parser);
+            var repoService = new LocalRepositoryService(repo, parser);
 
-            ////\\Htpc\d\Recorded TV
-            ////IEnumerable<WtvRecording> files = repoService.ScanFolder(@"\\TOWER\Media\Video\TV Soccer");
-            ////IEnumerable<WtvRecording> files = repoService.ScanFolder(@"\\Htpc\d\Recorded TV");
-            //IEnumerable<WtvRecording> files = repoService.ScanFolder(@"J:\Recorded TV");
-
-
-            //// var result = files.Where(p => p.ChannelNumber == "740");
-
-            //foreach (var item in files)
-            //{
-
-            //    Console.WriteLine("Output: {0} \t\n GUID: {1} \t\n BrodcastDate: {2} \n RecordingTime: {3} \n EpisodeName: {4}\n Length: {5}\n FileName: {6}\n FilePath: {7}\n Genre: {8}\n ProtectedRec: {9}\n Rerun: {10}\n Size: {11}\n StationCallSign {12}\n StationName {13}\n Subtitle {14} \n Description {15}",
-            //                                        item.Title,
-            //                                        item.WtvRecordingId, item.BroadcastDate, item.RecordingTime, item.EpisodeName,
-            //                                        item.Length, item.FileName, item.FilePath, item.Genre, item.ProtectedRecording,
-            //                                        item.Rerun, item.Size, item.StationCallSign, item.StationName, item.Subtitle, item.ProgramDescription
-            //                                        );
+            //\\Htpc\d\Recorded TV
+            //IEnumerable<WtvRecording> files = repoService.ScanFolder(@"\\TOWER\Media\Video\TV Soccer");
+            //IEnumerable<WtvRecording> files = repoService.ScanFolder(@"\\Htpc\d\Recorded TV");
+            IEnumerable<WtvRecording> files = repoService.ScanFolder(@"C:\RecordedTV");
 
 
-            //    //  context.WtvRecordings.Add(item);
-            //    //   context.SaveChanges();
+            // var result = files.Where(p => p.ChannelNumber == "740");
 
-            //}
+            foreach (var item in files)
+            {
+
+                Console.WriteLine("Output: {0} \t\n \t\n BrodcastDate: {1} \n RecordingTime: {2} \n EpisodeName: {3}\n Length: {4}\n FileName: {5}\n FilePath: {6}\n Genre: {7}\n ProtectedRec: {8}\n Rerun: {9}\n Size: {10}\n StationCallSign {11}\n StationName {12}\n Subtitle {13} \n Description {14}",
+                                                    item.Title,
+                                                    item.BroadcastDate, item.RecordingTime, item.EpisodeName,
+                                                    item.Length, item.FileName, item.FilePath, item.Genre, item.ProtectedRecording,
+                                                    item.Rerun, item.Size, item.StationCallSign, item.StationName, item.Subtitle, item.ProgramDescription
+                                                    );
 
 
-            //repoService.SaveToDatabase(files);
+                //  context.WtvRecordings.Add(item);
+                //   context.SaveChanges();
+
+            }
+
+
+            repoService.SaveToDatabase(files);
 
 
             Console.ReadLine();
